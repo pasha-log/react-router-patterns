19be5143606de051d6fa920454a7f9608a09f8b7
pasha-log <pasha.log@gmail.com>
2023-01-17 10:34:53 -0800

    Updates

---
+++ precache-manifest.08957483e89badc12718cd428de49e18.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "79ac400bfd938ad48652",
+    "url": "/react-router-patterns-dog-finder/static/css/main.741e19a6.chunk.css"
+  },
+  {
+    "revision": "79ac400bfd938ad48652",
+    "url": "/react-router-patterns-dog-finder/static/js/main.79ac400b.chunk.js"
+  },
+  {
+    "revision": "f0e0652fac49b8711ddc",
+    "url": "/react-router-patterns-dog-finder/static/css/1.a6228a37.chunk.css"
+  },
+  {
+    "revision": "f0e0652fac49b8711ddc",
+    "url": "/react-router-patterns-dog-finder/static/js/1.f0e0652f.chunk.js"
+  },
+  {
+    "revision": "ffc3ea2eb88a135d3d50",
+    "url": "/react-router-patterns-dog-finder/static/js/runtime~main.ffc3ea2e.js"
+  },
+  {
+    "revision": "5c1e3b17efc4e2197352567ea0e262c1",
+    "url": "/react-router-patterns-dog-finder/static/media/whiskey.5c1e3b17.jpg"
+  },
+  {
+    "revision": "c4647c75b42109bcb223348a50074b30",
+    "url": "/react-router-patterns-dog-finder/static/media/duke.c4647c75.jpg"
+  },
+  {
+    "revision": "2f5816ecbb1c2e849ee333f39067b3c5",
+    "url": "/react-router-patterns-dog-finder/static/media/perry.2f5816ec.jpg"
+  },
+  {
+    "revision": "2f47cb659ae081c9e07bc25c264f0aed",
+    "url": "/react-router-patterns-dog-finder/static/media/tubby.2f47cb65.jpg"
+  },
+  {
+    "revision": "1a985805c218adcb60a803cebb20ae19",
+    "url": "/react-router-patterns-dog-finder/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-router-patterns-color-factory/precache-manifest.d016d5a0b288713e8d8bc36202ab0941.js"
+  "/react-router-patterns-dog-finder/precache-manifest.08957483e89badc12718cd428de49e18.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-router-patterns-color-factory/index.html", {
+workbox.routing.registerNavigationRoute("/react-router-patterns-dog-finder/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
